091dc572e192e4bb0c0900461ff93fb4e78ac10a
nyxb <contact@nyxb.xyz>
2023-05-23 16:41:49 +0200

    🎉 feat(index-legacy.js): add WizardString, Bundle, and SourceMap classes to the export list This commit adds the WizardString, Bundle, and SourceMap classes to the export list of the index-legacy.js file. This allows for easier importing of these classes from other modules.

---
+++ src/index-legacy.js
@@ -0,0 +1,9 @@
+import WizardString from './WizardString.js'
+import Bundle from './Bundle.js'
+import SourceMap from './SourceMap.js'
+
+WizardString.Bundle = Bundle
+WizardString.SourceMap = SourceMap
+WizardString.default = WizardString
+
+export default WizardString
