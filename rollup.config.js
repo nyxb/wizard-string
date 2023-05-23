c1d4ae44a62565a0e92e59776cd7a3b3e7765364
nyxb <contact@nyxb.xyz>
2023-05-23 17:00:48 +0200

    🔧 chore(.eslintignore): add rollup.config.js to ignore list 🚀 feat(rollup.config.js): add rollup configuration for esm, cjs, and umd formats 🔧 chore(test): add .eslintrc and mocha.opts files for test configuration The ignore list is updated to exclude the rollup.config.js file from being linted. The rollup.config.js file is added to configure the rollup bundler to generate esm, cjs, and umd formats. The test configuration is added to include the .eslintrc and mocha.opts files for test configuration.

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import nodeResolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+
+const plugins = [
+   nodeResolve(),
+   replace({ DEBUG: false, preventAssignment: true }),
+]
+
+export default [
+   /* esm */
+   {
+      input: 'src/index.js',
+      external: ['@jridgewell/sourcemap-codec'],
+      plugins,
+      output: {
+         file: 'dist/wizard-string.es.mjs',
+         format: 'es',
+         exports: 'named',
+         sourcemap: true,
+      },
+   },
+
+   /* cjs */
+   {
+      input: 'src/index-legacy.js',
+      external: ['@jridgewell/sourcemap-codec'],
+      plugins,
+      output: {
+         file: 'dist/wizard-string.cjs.js',
+         format: 'cjs',
+         exports: 'default',
+         sourcemap: true,
+      },
+   },
+
+   /* umd */
+   {
+      input: 'src/index-legacy.js',
+      plugins,
+      output: {
+         file: 'dist/wizard-string.umd.js',
+         format: 'umd',
+         exports: 'default',
+         name: 'WizardString',
+         sourcemap: true,
+      },
+   },
+]
